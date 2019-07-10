36eb37a6e9482592c179a649661960d6f2ece994
hoaile1704 <hoaile1704@gmail.com>
2019-07-11 00:36:59 +0700

    update login && update user

---
+++ Angular/src/app/shared/api.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor() {}
+  baseurl = "https://localhost:44388/"
+  baseurlAPI = 'https://localhost:44388/api/';
+  Url = {
+    intake: this.baseurlAPI + 'INTAKE',
+    category: this.baseurlAPI + 'CATELORY',
+    catalog: this.baseurlAPI + 'CATALOG',
+    catalogs: this.baseurlAPI + 'CATALOGs',
+    semester: this.baseurlAPI + 'SEMESTER',
+    year: this.baseurlAPI + 'YEAR',
+    years: this.baseurlAPI + 'YEARs',
+    student: this.baseurlAPI + 'STUDENT',
+    program: this.baseurlAPI + 'PROGRAM',
+    typestudent: this.baseurlAPI + 'TYPE_STUDENT',
+    branch: this.baseurlAPI + 'BRANCH',
+    user: this.baseurlAPI + 'USER',
+    contact: this.baseurlAPI + 'CONTACT',
+    login: this.baseurl + "login",
+    changePassword : this.baseurlAPI + "ChangePassWord",
+    User : this.baseurlAPI + "myUser",
+    UpdateProfile : this.baseurlAPI + "updateUser"
+
+  };
+}
